8bd8588db03f1bdfcb0e95c45fcb506ece3111f7
Amr Keleg <amr_mohamed@live.com>
2021-12-29 13:09:24 +0000

    Refactor the weakly supervised model's interface

--- weakly_supervised_method/model.py
+++ weakly_supervised_method/model.py
@@ -1,31 +1,44 @@
 from snorkel.labeling import LFApplier, LFAnalysis
-from snorkel.labeling.model import MajorityLabelVoter, LabelModel
-from weakly_supervised_method.heuristics import lf_too_short, lf_is_a_date, lf_has_HTML
-from weakly_supervised_method.heuristics import (
-    lf_mostly_quotes,
-    lf_strange_ending,
-    lf_has_1st_or_2nd_person_pronoun,
-    lf_has_question_exclamation_marks,
-    lf_imperative_speech,
-    lf_is_repeated,
-)
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def votes_to_predictions(votes, threshold):
+    """Convert rows of votes into predictions"""
+
+    n_voting = (votes == 1).sum(axis=1)
+    predictions = n_voting >= threshold
+
+    return predictions
 
 
 class WeaklySupervisedMethod:
     def __init__(self, heuristics_list):
+        """Construct a weakly supervised method using a list of snorkel heuristics"""
         self.heuristics_list = heuristics_list
         self.applier = LFApplier(heuristics_list)
-        self.label_model = MajorityLabelVoter(cardinality=2)
 
-    def fit(self, dataset):
-        self.train_labels = self.applier.apply(dataset.articles)
-        return self.train_labels, self.label_model.predict(self.train_labels)
+    def fit(self, dataset, threshold=1):
+        """Fit the labelling functions using a dataset object"""
+        train_votes = self.applier.apply(dataset.articles)
+        logging.info(self.generate_lfs_report(train_votes))
 
-    def predict(self, dataset):
-        labels = self.applier.apply(dataset.articles)
-        return labels, self.label_model.predict(labels)
+        return train_votes, votes_to_predictions(train_votes, threshold)
 
-    def generate_train_report(self):
+    def predict(self, dataset, threshold=1):
+        """Generate predictions"""
+        votes = self.applier.apply(dataset.articles)
+        return votes, votes_to_predictions(votes, threshold)
+
+    def generate_lfs_report(self, votes):
         # Check the overlaps/conflicts between labeling functions
         # Note: No conflicts for now since negative labels aren't used
-        return LFAnalysis(L=self.train_labels, lfs=self.heuristics_list).lf_summary()
+        return LFAnalysis(L=votes, lfs=self.heuristics_list).lf_summary()
+
+    # TODO: Store/load the dataset to avoid repetitive computations
+    def pickle(self):
+        pass
+
+    def unpickle(self):
+        pass
